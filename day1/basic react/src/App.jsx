ecfa2ebd10b7b75473f0fc26f100f12a7397b084
Ayush Sharma <sahilsharma07a@gmail.com>
2026-07-13 21:08:34 +0530

    First day of learning react

---
+++ day1/basic react/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./test"
+function App() {
+
+  return (
+    <>
+      <h1 style={{ color: 'red' ,backgroundColor:'black'}}>Hello sahil sharma</h1>
+      <Test/>
+    </>
+  )
+} 
+
+export default App
---
+++ day1/basic react/src/test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return (
+        <h5 style={{color:'blue',backgroundColor:'green'}}>Welcome to react js course </h5>
+    )
+}
+
+export default Test
